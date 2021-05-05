9a7c4de220ae8f68886185cd9a9af60b9d0a2163
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 14:28:00 -0500

    Ejemplo 3 Practica 7

---
+++ Ejem3.java
@@ -0,0 +1,9 @@
+public class Ejem3 {
+    public static void main(String[] args){
+        int[] even_numbers = new int[]{ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        for (int number : even_numbers){
+            System.out.println("Numero ="+ number + "...");
+        }
+        return;
+    }
+}
