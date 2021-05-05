eff655ab649d2239808547e5f026906bab5855b5
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 12:57:25 -0500

    Ejemplo 1.5 PRactica 7

---
+++ Ejem1_5.java
@@ -0,0 +1,17 @@
+if ((number == null) && (number < 0)) {
+        System.out.println("El número está configurado");
+        } else {
+        if (number <3){
+            if(number ==1) {
+                 System.out.println("El número es uno");
+             } else if{
+                System.out.println("El número es dos");
+             }
+        } else {
+            if (number == 3){
+                System.out.println("El númmero es tres");
+            } else {
+                System.out.println("El munero es mayor que tres");
+            }
+        }
+}
