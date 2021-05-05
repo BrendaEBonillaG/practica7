8944a2e70bf1684480630d4ff43f33fcb382ae25
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 12:44:50 -0500

    Ejemplo 1.4 Practica 7

---
+++ Ejem1_4.java
@@ -0,0 +1,9 @@
+if (number == 1) {
+    System.out.println("El número es uno");
+        } else {
+    System.out.println("El número es dos");
+        } else if{
+    System.out.println("El número es tres");
+        } else {
+    System.out.println("El númmero es maayor que tres");
+        }
