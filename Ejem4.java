d56b8b3fb441f845e96cda84c86fbda60f98d5f2
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 14:31:31 -0500

    Ejemplo 4 Practica 7

---
+++ Ejem4.java
@@ -0,0 +1,11 @@
+public class Ejem4 {
+    public static void main(String[] args){
+        for (int n = 0; n < 15; n++){
+            System.out.println("Numero ="+ n + "...");
+            if (n == 5) {
+                break;
+            }
+        }
+        return;
+    }
+}
