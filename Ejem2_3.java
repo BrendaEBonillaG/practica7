f832b94f50b303c3ce907dae8eefee456d547fff
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 14:14:07 -0500

    Ejemplo 2.3 PRactica 7

---
+++ Ejem2_3.java
@@ -0,0 +1,17 @@
+public class Ejem2_3 {
+
+    public static void main(String[] args) {
+        int count = 10;
+        do {
+            System.out.println("Contando "+ count+ "...");
+            count--;
+        } while (count >0);
+        System.out.println("\n");
+
+        while (count < 10) {
+            System.out.println("Contando "+ count+ "...");
+            count++;
+        }
+        return;
+    }
+}
