8700200c41e166f46a411d0c53f82a3d7b69d791
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 13:20:41 -0500

    Ejemplo 1.6 Practica 7

---
+++ Ejem_6.java
@@ -0,0 +1,17 @@
+switch (number) {
+    case 1:
+        System.out.println("El numero es uno");
+        break;
+
+        case 2:
+        System.out.println("El numero es dos");
+        break;
+
+        case 3:
+        System.out.println("El numero es tres");
+        break;
+
+        default:
+        System.out.println("El numero es diferente a uno, dos o tres");
+        break;
+        }
