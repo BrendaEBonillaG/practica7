d26aa893cc718a8c239a82af59927856cc1c0f18
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 14:21:22 -0500

    Ejemplo 2.5 Practica 7

---
+++ Ejem2_5.java
@@ -0,0 +1,8 @@
+public class Ejem2_5 {
+    public static void main(String[] args) {
+        for (int count = 0; count < 20; count++ ) {
+            System.out.println("Contando" + count + "...");
+        }
+        return;
+        }
+}
