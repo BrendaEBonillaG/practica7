9d9beb2e4d047efe675bcf3cc9936e010eaf8c68
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-05 12:40:48 -0500

    Ejemplo 1.3 Practica 7

---
+++ Ejem1_3.java
@@ -0,0 +1,6 @@
+if ((name == null) || (name.equals(""))) { //Si name es null o está vacía
+    name = askName("Cual es tu nombre");
+    System.out.println("Hola"+name+"!");
+     } else {
+    Syste.out.println("Hola"+name+", bienvenido!");
+        }
